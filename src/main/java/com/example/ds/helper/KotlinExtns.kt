4d867673817055949114142516f0680246eb8f59
Santhosh <santhosh.kc@verse.in>
2019-05-22 08:46:14 +0530

    study

--- src/main/java/com/example/ds/helper/KotlinExtns.kt
+++ src/main/java/com/example/ds/helper/KotlinExtns.kt
@@ -8,23 +8,37 @@ fun <T> Array<T>?.isEmptyArray() = this?.isEmpty() ?: false
 
 fun IntArray?.isEmptyArray() = this?.isEmpty() ?: false
 
-fun <T> Array<T>?.swap(pos1 : Int?, pos2 : Int?) =
-        this?.let {
-            pos1 ?: return@let ; pos2 ?: return@let
-            if (it.ensureBounds(pos1) && it.ensureBounds(pos2)) {
-                val temp = this[pos1]
-                this[pos1] = this[pos2]
-                this[pos2] = temp
-            }
+fun <T> Array<T>?.swap(pos1: Int?, pos2: Int?) =
+    this?.let {
+        pos1 ?: return@let; pos2 ?: return@let
+        if (it.ensureBounds(pos1) && it.ensureBounds(pos2)) {
+            val temp = this[pos1]
+            this[pos1] = this[pos2]
+            this[pos2] = temp
         }
+    }
 
 fun <T> Array<Array<T>>?.swap(pos1: Pair<Int, Int>?, pos2: Pair<Int, Int>?) =
     this?.let {
-        pos1 ?: return@let ; pos2 ?: return@let
+        pos1 ?: return@let; pos2 ?: return@let
         if (it.ensureBounds(pos1.first, pos1.second) && it.ensureBounds(pos2.first, pos2.second)) {
             val temp = this[pos1.first][pos1.second]
             this[pos1.first][pos1.second] = this[pos2.first][pos2.second]
             this[pos2.first][pos2.second] = temp
+        }
+    }
+
+fun <T> Array<Array<T>>?.print() =
+    this?.let {
+        if (this[0].isEmptyArray()) return@let
+
+        for (i in 0 until this.size) {
+            for (j in 0 until this[0].size) {
+                System.out.print("${this[i][j].toString()}  ")
+            }
+            if (i < this.size - 1) {
+                System.out.println()
+            }
         }
     }
 
