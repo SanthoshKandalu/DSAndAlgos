b4a8e7c0c192f8c0e586fc0e6c21fc0463c2df12
Santhosh <santhosh.kc@verse.in>
2019-05-24 10:38:11 +0530

    study

---
+++ src/test/java/com/example/ds/test/LearningTest.kt
@@ -0,0 +1,39 @@
+package com.example.ds.test
+
+import com.example.ds.arrays.rotateMatrixInCyclicManner
+import com.example.ds.helper.print
+import org.junit.Test
+
+class LearningTest {
+    @Test
+    fun testRotatingArrayClockwise() {
+        rotateArray(arrayOf(arrayOf(1, 2, 3, 4), arrayOf(12, 13, 14, 5), arrayOf(11, 16, 15, 6), arrayOf(10, 9, 8, 7)))
+    }
+
+    @Test
+    fun testRotatingArrayAntiClockwise() {
+        rotateArray(arrayOf(arrayOf(1, 2, 3, 4), arrayOf(12, 13, 14, 5), arrayOf(11, 16, 15, 6), arrayOf(10, 9, 8, 7)), true)
+    }
+
+    @Test
+    fun testRotatingSingleRowArray() {
+        rotateArray(arrayOf(arrayOf(1,2,3,4)))
+    }
+
+    @Test
+    fun testRotatingSingleColumnArray() {
+        rotateArray(arrayOf(arrayOf(1), arrayOf(2), arrayOf(3), arrayOf(4)))
+    }
+
+    @Test
+    fun testRotatingEmptyArray() {
+        rotateMatrixInCyclicManner(arrayOf(arrayOf<Int>()), true)
+    }
+
+    private fun rotateArray(array : Array<Array<Int>>, antiClockwise : Boolean = false) {
+        array.print()
+        System.out.println("-----------------")
+        rotateMatrixInCyclicManner(array, antiClockwise)
+        array.print()
+    }
+}
