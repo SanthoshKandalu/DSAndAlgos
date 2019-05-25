0b775f40c41def2eef0534e160512b0d8db265bc
Santhosh <santhosh.kc@verse.in>
2019-05-25 10:03:19 +0530

    study

--- src/test/java/com/example/ds/test/MatrixTest.kt
+++ src/test/java/com/example/ds/test/MatrixTest.kt
@@ -1,10 +1,27 @@
 package com.example.ds.test
 
+import com.example.ds.arrays.printMatrixInCyclicManner
 import com.example.ds.arrays.rotateMatrixInCyclicManner
 import com.example.ds.helper.print
 import org.junit.Test
 
 class MatrixTest {
+
+    @Test
+    fun testPrintArrayInCyclicMannerAntiClockwise() {
+        printMatrixInCyclicManner(arrayOf(arrayOf(1,10,9),arrayOf(2,11,8), arrayOf(3,12,7), arrayOf(4,5,6)), true)
+    }
+
+    @Test
+    fun testPrintArrayInCyclicMannerClockwise() {
+        printMatrixInCyclicManner(arrayOf(arrayOf(1,2,3,4),arrayOf(12,13,14,5), arrayOf(11,16,15,6), arrayOf(10,9,8,7)))
+    }
+
+    @Test
+    fun testPrintEmptyArrayInCyclicManner() {
+        printMatrixInCyclicManner(arrayOf(arrayOf()))
+    }
+
     @Test
     fun testRotatingArrayClockwise() {
         rotateArray(arrayOf(arrayOf(1, 2, 3, 4), arrayOf(12, 13, 14, 5), arrayOf(11, 16, 15, 6), arrayOf(10, 9, 8, 7)))
